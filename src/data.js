f5b4a59c93b228516d61dad8606e235bf3420a2e
T Omoto <tyler.omoto@gmail.com>
2022-09-12 21:30:45 -0400

    Base foundation deploy

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const projects = [
+  {
+    title: "Stocker Inventory Management",
+    subtitle: "MERN Stack",
+    description:
+      "This project is a Full-Stack inventory management system that integrates seamlessly with a fully functional eCommerce site.",
+    image: "./project-1.gif",
+    link: "https://stockerinventory.herokuapp.com/",
+    repo: "https://github.com/SuedePritch/furry-leftover-broccoli"
+  },
+  {
+    title: "Express Movies",
+    subtitle: "MySQL, Nodejs, Express",
+    description:
+      "This project uses a created database for users to review and discuss movies currently in theatres (Canada).",
+    image: "./project-2.gif",
+    link: "https://vast-shelf-82768.herokuapp.com/",
+    repo: "https://github.com/WilliamL1998/crispy-octo-bassoon",
+  },
+  {
+    title: "BreweryNearMe",
+    subtitle: "HTML, CSS and API",
+    description:
+      "This project uses a 3rd party API to search for a Brewery near the users location, their past search history will also be displayed on the left!",
+    image: "./project-3.gif",
+    link: "https://tyomoto.github.io/BreweryNearMe/",
+    repo: "https://github.com/tyomoto/BreweryNearMe/",
+  },
+  {
+    title: "Employee Tracker",
+    subtitle: "JavaScript, MySQL",
+    description:
+      "This project is a command line app that helps to manage a company's database of employees.",
+    image: "./project-4.gif",
+    link: "https://github.com/tyomoto/SQL-Employee-Tracker/",
+    repo: "https://github.com/tyomoto/SQL-Employee-Tracker/",
+  },
+  {
+    title: "Team Profile Generator",
+    subtitle: "ExpressJs, MySQL, Javascript",
+    description:
+      "This project is a command link app that will generate an HTML page, which displays user-inputted data about their software engineering team.",
+    image: "./TeamProfileGenerator.png",
+    link: "https://github.com/tyomoto/cautious-goggles",
+    repo: "https://github.com/tyomoto/cautious-goggles",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "TEST EXMAMPLE TESTIMONY",
+    image: "https://randomuser.me/api/portraits/men/8.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "TEST EXMAMPLE TESTIMONY",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Express.js",
+  "Node",
+  "MySQL",
+  "Material UI",
+  "MongoDB",
+];
